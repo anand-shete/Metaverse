94abff8f31889fdb0e8e2fc7c39c770135b82538
anand-shete <anandshete1234@gmail.com>
2026-08-21 15:49:52 +0530

    fix: switched to node:alpine image to reduce AWS ECR storage

--- apps/api/src/config/index.config.ts
+++ apps/api/src/config/index.config.ts
@@ -2,4 +2,4 @@ export * from "./db.config";
 export * from "./env.config";
 export * from "./aws.config";
 export * from "./groq.config";
-export * from "./logger.config"
+export * from "./logger.config";
